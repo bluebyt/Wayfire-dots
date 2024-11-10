bad61cc1a5b6ad441fa20d54d82b5bd3b5c48445
bluebyt <bluebyt@gmail.com>
2024-11-10 09:25:11 -0500

    Add files via upload

--- config/waybar/modules/zscroll.sh
+++ config/waybar/modules/zscroll.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 zscroll --before-text " x" --delay 0.3 \
+    --length 20 \
 		--match-command "mpc status" \
-		--match-text "playing" "--before-text ' '" \
-		--match-text "paused" "--before-text ' ' --scroll 0" \
+		--match-text "playing" "--before-text ' ' --scroll 1" \
+		--match-text "paused" "--before-text ' ' --scroll 0" \
+		--update-interval 1 \
 		--update-check true "mpc current" &
 
 wait
-
