c8790e39f786bf7dbfa4ace5a1701770b492e7ad
bluebyt <bluebyt@gmail.com>
2024-11-23 06:40:31 -0500

    Delete config/ipc-scripts directory

--- config/ipc-scripts/wayfire_work_prev.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from wayfire import WayfireSocket
-from wayfire.extra.ipc_utils import WayfireUtils
-import itertools
-sock = WayfireSocket()
-utils = WayfireUtils(sock) 
-utils.go_previous_workspace()
