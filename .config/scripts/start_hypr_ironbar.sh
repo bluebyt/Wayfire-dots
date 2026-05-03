b6574565fcb004e41381308223a7b267e97fabe8
bluebyt <bluebyt@gmail.com>
2026-05-03 05:50:14 -0400

    Update Ironbar startup script

---
+++ .config/scripts/start_hypr_ironbar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export IRONBAR_CONFIG=/home/bluebyt/.config/ironbar/config_hypr.corn
+export IRONBAR_CSS=/home/bluebyt/.config/ironbar/style_hypr_nord.css
+
+exec /home/bluebyt/.local/bin/ironbar
---
+++ .config/scripts/start_way_ironbar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export IRONBAR_CONFIG=/home/bluebyt/.config/ironbar/config_way.corn
+export IRONBAR_CSS=/home/bluebyt/.config/ironbar/style_way_nord.css
+
+exec /home/bluebyt/.local/bin/ironbar
